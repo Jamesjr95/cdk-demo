c558726c51298ef010d2a3ecb14d2b7e05c69b69
James Johnson <jamesjhnsn1995@gmail.com>
2023-06-15 21:09:41 -0700

    finished demo

--- README.md
+++ README.md
@@ -1,14 +1,6 @@
-# Welcome to your CDK TypeScript project
+# Project Title
+CDK Demo
 
-This is a blank project for CDK development with TypeScript.
+## Description
 
-The `cdk.json` file tells the CDK Toolkit how to execute your app.
-
-## Useful commands
-
-* `npm run build`   compile typescript to js
-* `npm run watch`   watch for changes and compile
-* `npm run test`    perform the jest unit tests
-* `cdk deploy`      deploy this stack to your default AWS account/region
-* `cdk diff`        compare deployed stack with current state
-* `cdk synth`       emits the synthesized CloudFormation template
+This project provisions AWS resources using AWS CDK. It creates a Virtual Private Cloud (VPC) with two public-facing subnets, spread across two availability zones.
--- lib/cdk-demo-stack.ts
+++ lib/cdk-demo-stack.ts
@@ -1,13 +1,26 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+// Importing the EC2 Module
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
 
 export class CdkDemoStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
-
+    // we have created the VPC object from the vpc class
+    new ec2.Vpc(this, 'mainVPC', {
+      // This is where you can define how many AZs you want to use
+      maxAzs: 2,
+      // This is where you can define the subnet configuration per AZ
+      subnetConfiguration: [
+        {
+          cidrMask: 24,
+          name: 'public-subnet',
+          subnetType: ec2.SubnetType.PUBLIC,
+        }
+      ]
+    })
     // example resource
     // const queue = new sqs.Queue(this, 'CdkDemoQueue', {
     //   visibilityTimeout: cdk.Duration.seconds(300)
